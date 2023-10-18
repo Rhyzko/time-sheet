f7df190a1c5e90693bf39668b0c440d3183136a9
Rhyz <valentin.martin@outlook.com>
2023-10-18 19:00:37 +0200

    add layout and settings page

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="text-center flex items-center align-middle">
+        <NuxtLink to="/">Default header content</NuxtLink>
+        <NuxtLink to="/settings">
+            <UIcon name="i-heroicons-cog-6-tooth" class="ml-auto" @click=""></UIcon>
+        </NuxtLink>
+    </header>
+    <main>
+        <slot />
+    </main>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pages/settings.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        Here are the settings
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
