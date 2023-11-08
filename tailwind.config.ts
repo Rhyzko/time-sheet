6054d93566b9514ba649508e091e0d3aa3887678
Rhyz <valentin.martin@outlook.com>
2023-11-08 12:01:45 +0100

    safelist colors

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+    safelist: [
+        'dark:bg-orange-900',
+        'dark:bg-slate-800',
+        'dark:bg-slate-600',
+        'dark:bg-red-900',
+        'dark:border-white-200',
+        'dark:divide-gray-800',
+        'dark:ring-gray-800',
+        {
+            pattern: /bg-(orange|green|slate|gray)-(100|200|300|400|500|600|800|900)/,
+        }
+    ],
+}
